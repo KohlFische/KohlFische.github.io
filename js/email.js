5515622cc177fad062083a5071b3890f322c075a
wuorbs <wuorbs@gmail.com>
2023-08-02 20:33:12 +0800

    Site updated: 2023-08-02 20:33:12

---
+++ js/email.js
@@ -0,0 +1,6 @@
+// async function copyEmail() {
+//     await navigator.clipboard.writeText('wuorbs@gmail.com')
+//     btf.snackbarShow('Email copied!')
+// }
+
+document.querySelector('a[title="Email"]').target = "_self"
