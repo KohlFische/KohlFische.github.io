ac5d305c4f82601286d26672565605f0bd23736f
wuorbs <wuorbs@gmail.com>
2023-08-03 17:49:18 +0800

    Site updated: 2023-08-03 17:49:18

--- js/email.js
+++ js/email.js
@@ -2,6 +2,8 @@ document.querySelectorAll('.social-icon[title="Email"]').forEach(element => {
     element.target = '_self'
 })
 
+// document.querySelector('.social-icon[title="Email"]').target = '_self'
+
 async function copyEmail() {
     await navigator.clipboard.writeText('wuorbs@gmail.com')
     btf.snackbarShow('Email copied!')
